14ee9f445c52f526ea09802139284951b3d6625e
kwon <kwon@kwonui-mbp>
2015-04-04 01:43:49 +0900

    중간 commit

--- avalon/src/webapp/public/js/appsControllers.js
+++ avalon/src/webapp/public/js/appsControllers.js
@@ -1,5 +1,37 @@
 var appsControllers = angular.module('appsControllers', []);
-
-appsControllers.controller('appsCtrl', ['$scope', '$http', '$interval',  function($scope, $http, $interval) {
-    console.log("loading apps");
+appsControllers.factory('Agents', function ($resource) {
+    var Agents = $resource('http://192.168.43.146:20308/status', null, {
+    	query: {method: 'GET', isArray: false }
+    });
+    return Agents;
+  });
+appsControllers.controller('agentsCtrl', ['$scope', '$http', '$interval', 'Agents', function($scope, $http, $interval, Agents) {
+	console.log("Agents call..");
+	$scope.agentInfo = Agents.query(function(agent){ // 호출시 추가할 파라미터는 없고 {}, 성공 시 콜백 추가 
+      console.log(agents.name + ", " + agents.url + "");
+    });
+    
+//    $scope.agents = [
+//                     {
+//                    	 name: "Agent1",
+//                    	 url: "192.168.43.146:20308",
+//                    	 path: "path",
+//                    	 cpu: "cpu",
+//                    	 disk: "disk"
+//                     },
+//                     {
+//                    	 name: "Agent2",
+//                    	 url: "192.168.43.146:20308",
+//                    	 path: "path",
+//                    	 cpu: "cpu",
+//                    	 disk: "disk"
+//                     },
+//                     {
+//                    	 name: "Agent3",
+//                    	 url: "192.168.43.146:20308",
+//                    	 path: "path",
+//                    	 cpu: "cpu",
+//                    	 disk: "disk"
+//                     }
+//                     ];
 }]);
